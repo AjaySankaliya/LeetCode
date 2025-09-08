a71c1158515f8d93db09fb1ba100c1a2d5366690
Ajay Sankaliya <120821791+AjaySankaliya@users.noreply.github.com>
2025-09-08 19:01:28 +0530

    leetcode 283

---
+++ Array/Moves_Zero_283.java
@@ -0,0 +1,29 @@
+class Moves_Zero_283
+{
+    public static void main(String[] args) {
+        
+        int arr[]={0, 1, 0, 3, 12};
+        moveZeroes(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void moveZeroes(int[] arr)
+    {
+        int lb=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]!=0)
+            {
+                arr[lb]=arr[i];
+                lb++;
+            }
+        }
+        while(lb<arr.length)
+        {
+            arr[lb]=0;
+            lb++;
+        }
+    }
+}
